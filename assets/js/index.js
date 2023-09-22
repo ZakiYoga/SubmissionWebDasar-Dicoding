d0ceaebf5f924c3438306ee6f83fbca16b6dd3d8
ZakiYoga <l200200229@student.ums.ac.id>
2023-09-22 19:05:37 +0700

    donee

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,25 @@
 const navbar = document.querySelector('.header-navbar');
+const drop_down = document.getElementById('btn_dropdown')
+const drop_downContent = document.getElementById('dropdown_content');
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) { // Ubah angka 50 sesuai dengan posisi scroll yang Anda inginkan
-        navbar.style.backgroundColor = 'rgba(20, 108, 148, 0.9)'; // Ganti warna latar belakang navbar saat digulir
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = 'rgba(20, 108, 148, 0.9)';
     } else {
-        navbar.style.backgroundColor = 'transparent'; // Buat navbar transparan saat di atas
+        navbar.style.backgroundColor = 'transparent';
+    }
+});
+
+drop_down.addEventListener('click', () => {
+    if (drop_downContent.style.display === 'block') {
+        drop_downContent.style.display = 'none';
+    } else {
+        drop_downContent.style.display = 'block';
+    }
+});
+
+document.addEventListener('click', (event) => {
+    if (event.target !== drop_down && event.target !== drop_downContent) {
+        drop_downContent.style.display = 'none';
     }
 });
